4e07138b3570b55fece580700a7acfef74e75073
Chenhao Ma <machenhao@gmail.com>
2016-02-20 23:25:18 +0800

    Backup

--- mergemaster.sh
+++ mergemaster.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+cd ~/Kancolle/74eo.makai
 git add -A
 git commit -m Backup
 git co master
@@ -7,6 +8,6 @@ git pull
 git co tablet
 git merge --no-commit -X theirs master
 git co Settings/Configuration.xml
-git commit
+git commit -m "Merge branch 'master' into tablet"
 git push
 
