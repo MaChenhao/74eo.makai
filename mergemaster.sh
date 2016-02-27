921499b3f97a62e5f6518210cba003ff087537bd
Chenhao Ma <machenhao@gmail.com>
2016-02-27 22:15:55 +0800

    Backup

--- mergemaster.sh
+++ mergemaster.sh
@@ -7,6 +7,7 @@ git pull
 git co tablet
 git merge --no-commit -X theirs master
 git co Settings/Configuration.xml
+git co Settings/WindowLayout2.zip
 git commit -m "Merge branch 'master' into tablet"
 git push
 
