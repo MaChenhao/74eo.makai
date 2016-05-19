8d520acfd8f1828401a11d273606f78a52593add
Chenhao Ma <machenhao@gmail.com>
2016-05-19 20:19:31 +0800

    Backup

--- mergemaster.sh
+++ mergemaster.sh
@@ -2,13 +2,13 @@
 
 cd ~/Kancolle/74eo.makai || exit $?
 git add -A || exit $?
-git commit -a -m Backup || exit $?
+git commit -a -m Backup
 git co master || exit $?
 git pull || exit $?
 git co tablet || exit $?
 git merge --no-commit -X theirs master || exit $?
 git co Settings/Configuration.xml || exit $?
 git co Settings/WindowLayout2.zip || exit $?
-git commit -m "Merge branch 'master' into tablet" || exit $?
+git commit -m "Merge branch 'master' into tablet"
 git push
 
