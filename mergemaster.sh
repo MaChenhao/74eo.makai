19d66c3f15da244ce78ac034661e1a9768ab4b97
Chenhao Ma <machenhao@gmail.com>
2016-05-19 20:17:02 +0800

    Backup

--- mergemaster.sh
+++ mergemaster.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
-# cd ~/Kancolle/74eo.makai
-git add -A || (c=$?; (exit $c))
-git commit -a -m Backup || (c=$?; (exit $c))
-git co master || (c=$?; (exit $c))
-git pull || (c=$?; (exit $c))
-git co tablet || (c=$?; (exit $c))
-git merge --no-commit -X theirs master || (c=$?; (exit $c))
-git co Settings/Configuration.xml || (c=$?; (exit $c))
-git co Settings/WindowLayout2.zip || (c=$?; (exit $c))
-git commit -m "Merge branch 'master' into tablet" || (c=$?; (exit $c))
+cd ~/Kancolle/74eo.makai || exit $?
+git add -A || exit $?
+git commit -a -m Backup || exit $?
+git co master || exit $?
+git pull || exit $?
+git co tablet || exit $?
+git merge --no-commit -X theirs master || exit $?
+git co Settings/Configuration.xml || exit $?
+git co Settings/WindowLayout2.zip || exit $?
+git commit -m "Merge branch 'master' into tablet" || exit $?
 git push
 
